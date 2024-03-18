92174e09d642575ea52045f2365033459a9d5f81
Gmongtor <gmongtor@myuax.com>
2024-03-18 19:13:56 +0100

    17

--- Ecosistema/src/main/java/SeguridadSistema/GestorAutenticacion.java
+++ Ecosistema/src/main/java/SeguridadSistema/GestorAutenticacion.java
@@ -1,5 +1,7 @@
 package SeguridadSistema;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,16 +10,24 @@ public class GestorAutenticacion {
 
     public boolean iniciarSesion(String nombreUsuario, String contraseña) {
         Usuario usuario = usuariosRegistrados.get(nombreUsuario);
-        if (usuario != null && usuario.getContraseñaHash().equals(contraseña.hashCode())) {
-            // La autenticación es exitosa
-            return true;
+        if (usuario != null) {
+            try {
+                return usuario.verificarContraseña(contraseña);
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                e.printStackTrace(); // En un caso real, manejar apropiadamente la excepción
+            }
         }
         return false;
     }
 
     public void registrarUsuario(String nombreUsuario, String contraseña, String rol) {
-        usuariosRegistrados.put(nombreUsuario, new Usuario(nombreUsuario, String.valueOf(contraseña.hashCode()), rol));
+        try {
+            usuariosRegistrados.put(nombreUsuario, new Usuario(nombreUsuario, contraseña, rol));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace(); // En un caso real, manejar apropiadamente la excepción
+        }
     }
 }
+
 
 

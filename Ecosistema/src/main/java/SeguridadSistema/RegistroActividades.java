242635c7defb8b340fa550a950de53cad27d6098
Gmongtor <gmongtor@myuax.com>
2024-03-18 11:02:41 +0100

    8

---
+++ Ecosistema/src/main/java/SeguridadSistema/RegistroActividades.java
@@ -0,0 +1,4 @@
+package SeguridadSistema;
+
+public class RegistroActividades {
+}
---
+++ Ecosistema/src/main/java/SeguridadSistema/Simulador.java
@@ -0,0 +1,18 @@
+package SeguridadSistema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Autentificacion {
+    private Map<String, String> usuarios = new HashMap<>();
+
+    public Autentificacion() {
+        // Ejemplo de usuario predeterminado, en práctica usa contraseñas cifradas
+        usuarios.put("investigador", "12345");
+    }
+
+    public boolean autenticar(String usuario, String contraseña) {
+        return usuarios.containsKey(usuario) && usuarios.get(usuario).equals(contraseña);
+    }
+}
+

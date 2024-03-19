dd0e1bb311ebc79a7a1bca157fa2a0d04a7fc964
Gmongtor <gmongtor@myuax.com>
2024-03-19 14:26:48 +0100

    26

--- Ecosistema/src/main/java/Entidades/Organismo.java
+++ Ecosistema/src/main/java/Entidades/Organismo.java
@@ -5,7 +5,7 @@ public abstract class Organismo {
     protected double salud;
     protected int edad;
     protected boolean estadoReproductivo;
-    protected boolean vivo = true; // Nuevo campo para indicar si el organismo está vivo
+    protected boolean vivo = true;
 
     public Organismo(int posX, int posY, double salud, int edad, boolean estadoReproductivo) {
         this.posX = posX;
@@ -27,8 +27,57 @@ public abstract class Organismo {
         this.vivo = false;
     }
 
-    // Getters y Setters para posX, posY, salud, edad, estadoReproductivo, y vivo
+    // Getters
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public double getSalud() {
+        return salud;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean isEstadoReproductivo() {
+        return estadoReproductivo;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    // Setters
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public void setSalud(double salud) {
+        this.salud = salud;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setEstadoReproductivo(boolean estadoReproductivo) {
+        this.estadoReproductivo = estadoReproductivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
 }
+
 
 
 
